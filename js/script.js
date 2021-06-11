da36dd564601703638811a00f72c02ea665c4eef
maicol <maicol.matteoli@gmail.com>
2021-06-11 14:49:34 +0200

    add  : verifica dei numeri trovati e output

--- js/script.js
+++ js/script.js
@@ -16,8 +16,23 @@ timer();
 
 
 function timer(){
+    var n; 
+    var nTrovati = []; //array dei numeri indovinati dall'utente
+
     var t = setTimeout(function(){
-        alert("timeout");
+        // alert("timeout");
+        alert("scrivi tutti e 5 i numeri !!")
+        for(var i=0 ; i<5; i++){
+            n = parseInt(prompt("inserisci numero : "));
+            console.log(n);
+            if(numbers.includes(n)){
+                nTrovati.push(n);
+            }
+            else{
+                console.log("numero non trovato");
+            }
+        }
+        alert("hai indovinato " + nTrovati.length + " numeri  : " + nTrovati); //visualizzo quanti e quali numeri l'utente ha indovinato
 
     },5000);
 }
