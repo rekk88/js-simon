fecfb3c0d8723344134633d7ac506343d4970351
maicol <maicol.matteoli@gmail.com>
2021-06-11 14:40:21 +0200

    test : timeout

--- js/script.js
+++ js/script.js
@@ -1,27 +1,28 @@
 var min = 1;
 var max = 100;
 var random;
-for(var i=0 ; i <= 100 ; i++){
-     random = rand(min, max);
-     console.log(random);
-
+var numbers = [];
+//genero 5 numeri random
+for(var i=0 ; i < 5 ; i++){
+    random = rand(min, max);
+    numbers.push(random); 
 }
-console.log("secondo random");
-// for(var i=0 ; i <= 100 ; i++){
-//     random = rand();
-//     console.log(random);
+console.log(numbers);
+alert(numbers); //output di 5 numeri
 
-// }
+timer();
 
 
 
 
+function timer(){
+    var t = setTimeout(function(){
+        alert("timeout");
 
-function rand(max , min){
+    },5000);
+}
+
+
+function rand(min , max){
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
-
-// function rand(){
-//     return Math.floor(Math.random() * 100 + 1);
-// }
-
